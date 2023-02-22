1876e869c357187aa88bd76f1314d76ff4b5456c
pgazmuri <pablo@gazmuri.net>
2023-02-22 10:16:53 -0500

    Initial Commit
    Basic implementation for Waffle blocking

---
+++ WaffleBlock/content.js
@@ -0,0 +1,11 @@
+const overlayDiv = document.createElement('div');
+overlayDiv.style.position = 'fixed';
+overlayDiv.style.top = '0';
+overlayDiv.style.left = '0';
+overlayDiv.style.width = '48px';
+overlayDiv.style.height = '48px';
+overlayDiv.style.background = 'transparent';
+overlayDiv.style.zIndex = '9999';
+
+document.body.appendChild(overlayDiv);
+
